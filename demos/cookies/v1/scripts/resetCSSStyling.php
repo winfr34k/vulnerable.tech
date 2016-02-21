11a0cba17ce175a11cb6c5c8ac4a3852c7a08797
Thomas Keppler <winfr34k@gmail.com>
2016-02-21 01:19:18 +0100

    Cookie Injection v1: Make the resetCSSStyling actually work

--- demos/cookies/v1/scripts/resetCSSStyling.php
+++ demos/cookies/v1/scripts/resetCSSStyling.php
@@ -1,7 +1,6 @@
 <?php
   //Reset cookies by setting their "best before" date to an hour ago
-  setcookie('color', '', time() - 3600, '/');
-  setcookie('text-decoration', '', time() - 3600, '/');
-
-  //Redirect back - Sheesh! I should find a better solution.
-  header('Location: ' . $_SERVER['HTTP_REFERER']);
+  if (setcookie('color', '', time() - 3600, '/') && setcookie('text-decoration', '', time() - 3600, '/')) {
+    //Redirect back - Sheesh! I should find a better solution.
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+  }
